23369fef4ad89259f512c569b856fbcb6eb2e36d
Milan Thapa <milans.thapa78@gmail.com>
2017-12-20 23:45:45 +0545

    lol, adding the compose file

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 bundle check || bundle install
-sleep 10m
-# bundle exec shotgun --server=webrick --port=4567 --host=0.0.0.0 --env=production web.rb
 
-bundle exec ruby web.rb -p 4567
+# bundle exec shotgun --server=webrick --port=4567 --host=0.0.0.0 --env=production web.rb
+bundle exec ruby web.rb
